65ecb212c437b7951ac07276ba6e01b1505e7f4a
Katafotic <biga.vs@edu.spbstu.ru>
2020-03-18 15:09:46 +0300

    Final

--- C2502h.c
+++ C2502h.c
@@ -1,40 +1,24 @@
+//C2502h
+//3430302/90003
+//Biga V.S.
+//18.03.2020
+//
 #include <stdlib.h>
 #include <stdio.h>
-void vecSwap(const int iLength, int *iVec)
-{
-  int i, marker;
-  if (iLength==1) return;
-  else marker=(int)iLength/2 - 1;
-  for(i=0; i<=marker; i++)
-    {
-      if ( *(iVec+i)==*(iVec+iLength-1-i) ) continue;
-      else {
-            *(iVec+i) ^ *(iVec+iLength-1-i);
-            *(iVec+i) ^ *(iVec+iLength-1-i);
-            *(iVec+i) ^ *(iVec+iLength-1-i);
-           }
-    }
-  return;
-}
-void printVector(const int iLength, int *iVec)
-{
-  int i,tmp;
-  for (i=0; i<iLength; i++)
-    {
-      tmp=*(iVec+i);
-      //printf("%i \t", *(iVec+i) );
-      printf("%i \t", tmp);
-    }
-}
+
+void vecSwap(const int iLength, int *iVec);
+void printVector(const int iLength, int *iVec);
+
 int main()
 {
-  int tmp, num=-1;
+  int tmp, num=0;
   FILE* inf;
+  char Ch_tmp;
 
   printf("C2502h\n");
   printf("3430302/90003\n");
   printf("Biga V.S.\n");
-  printf("17.03.2020\n");
+  printf("18.03.2020\n");
   printf("\n");
 
   inf = fopen("inputC2502h.txt", "rt");
@@ -44,29 +28,59 @@ int main()
       return -1;
     }
 
-  do
+  int fl=1;
+  do // считыввает число элементов
     {
-      fscanf(inf, "%i", &tmp);
-      num = num+1;
+      fscanf(inf, "%c", &Ch_tmp);
+      if (Ch_tmp<='9' && Ch_tmp>='0' )
+        {
+              if (fl==1) // fl=1 если предыдущий символ -табуляция
+                {
+                  num = num + 1;
+                  fl = 0;
+                }
+        }
+      else fl=1;
     } while (!feof(inf));
-    printf("%i \n",num );
-  //  т.к всегда происходит 1 итерация
+
   fclose(inf);
   if (num==0)
     {
       printf("%s\n","ERROR. Clean file." );
       return 1;
     }
-  printf("%s\n","File will open" );
-  inf = fopen("inputC2205h.txt", "rt");
+  inf = fopen("inputC2502h.txt", "rt");
   int Vector[num];
-  printf("%s \n", "Vector type was created");
-  //  впоследующем цикле ошибка - segmentation fauld
-  for (tmp=0; tmp < num; tmp++)   fscanf(inf, "%i", &Vector[0]+tmp);
-  printf("%s\n","File will open" );
+
+  for (tmp=0; tmp < num; tmp++)     fscanf(inf, "%i", &Vector[tmp]);
+  fclose(inf);
+  printf("\n");
 
   printVector(num, &Vector[0]);
   vecSwap (num, &Vector[0]);
   printVector(num, &Vector[0]);
-  return 1;
+  return 0;
+}
+
+void vecSwap(const int iLength, int *iVec)
+{
+  int i, marker;
+  if (iLength==1) return;
+  else marker=(int)iLength/2 - 1;
+  for(i=0; i<=marker; i++)
+    {
+      if ( *(iVec+i)==*(iVec+iLength-1-i) ) continue;
+      else {
+            *(iVec+i) = *(iVec+i)  ^  *(iVec+iLength-1-i);
+            *(iVec+iLength-1+i) = *(iVec+i)  ^  *(iVec+iLength-1-i);
+            *(iVec+i) = *(iVec+i)  ^  *(iVec+iLength-1-i);
+           }
+    }
+  return;
+}
+void printVector(const int iLength, int *iVec)
+{
+  int i;
+  for (i=0; i<iLength; i++)    printf("%i \t", *(iVec+i) );
+  printf("\n");
 }
